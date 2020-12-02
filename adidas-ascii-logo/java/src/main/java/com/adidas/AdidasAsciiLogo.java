6e3224020227e59f4da9822afb911c7d00bef350
William Rabbermann <Sir.Mongooser@gmail.com>
2020-12-02 12:55:38 -0600

    Created solution in Java
    Added my solution to the AdidasAsciiLogoJava challenge. Fun!!

--- adidas-ascii-logo/java/src/main/java/com/adidas/AdidasAsciiLogo.java
+++ adidas-ascii-logo/java/src/main/java/com/adidas/AdidasAsciiLogo.java
@@ -1,9 +1,68 @@
 package com.adidas;
+import java.lang.Math;
 
 public class AdidasAsciiLogo {
 
-    public String createAdidasAsciiLogo(int width) {
-        return null;
-
+    public static String createAdidasAsciiLogo(int width) {
+        if (width < 2)
+            return "Error, minimum width is 2";
+        String result = "";
+        int height = (int)Math.ceil(Math.sqrt(width));
+        int step = height; 
+        
+        for ( int h=0; h < height * 3; h++ ) {
+            String line = "";
+            
+            for (int w=h+1; w < step; w++) {
+                line += ' ';
+            }
+            
+            if (h >= height) {
+                for (int w=0; w < width; w++) {
+                    line += ' ';
+                }
+            } else {
+                for (int a=0; a < width; a++) {
+                    line += '@';
+                }
+            }
+            if (h >= height) {
+                for (int w=h - height; w < step - 1; w++) {
+                    line += ' ';
+                }
+            } else {
+                for (int w=0; w < step; w++) {
+                    line += ' ';
+                }
+            }
+            
+            if (h >= height * 2) {
+                for (int w=0; w < width; w++) {
+                    line += ' ';
+                }
+            } else {
+                for (int a=0; a < width; a++) {
+                    line += '@';
+                }
+            }
+            
+            if (h >= height * 2) {
+                for (int w=h - height*2; w < step-1; w++) {
+                    line += ' ';
+                }
+            } else {
+                for (int w=0; w < step; w++) {
+                    line += ' ';
+                }
+            }
+            
+            for (int a=0; a < width; a++) {
+                line += '@';
+            }
+            line += '\n';
+            result = line + result; // Add the line before so as to work from bottom up.
+        }
+        
+        return result;
     }
 }
